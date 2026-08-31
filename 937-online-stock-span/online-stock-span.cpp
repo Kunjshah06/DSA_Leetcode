205910ebcfacdbb359ff02b987e72ec2dade959b
Kunjshah06 <202511031@dau.ac.in>
2026-08-31 17:39:51 +0530

    Time: 14 ms (98.69%) | Memory: 97.1 MB (15.59%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.cpp
@@ -0,0 +1,22 @@
+class StockSpanner {
+private:
+    stack<pair<int, int>> st;  // {price, span}
+
+public:
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int span = 1;
+        
+        // Merge previous smaller or equal prices
+        while (!st.empty() && st.top().first <= price) {
+            span += st.top().second;
+            st.pop();
+        }
+        
+        st.push({price, span});
+        return span;
+    }
+};
